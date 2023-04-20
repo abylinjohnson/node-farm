a6284b66d758843dbdf70606f13c6a5bf518cd77
abylinjohnson <jgabylin@karunya.edu.in>
2023-04-21 00:37:27 +0530

    changes data

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
 const fs = require('fs');
 
-
-
-//Blocking, synchronous way
-// const textIn = fs.readFileSync('./txt/input.txt','utf-8')
-// console.log(textIn)
-// const textOut = `This is what we know about the avacado: ${textIn}. \n Created on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log("File written successfully");
-
-//Non-Blocking asynchronously
-// fs.readFile('./txt/start.txt','utf-8',(err, data1) => {
-//     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err, data2) =>{
-//         console.log(data2);
-//         fs.readFile(`./txt/append.txt`,'utf-8',(err, data3) =>{
-//             console.log(data3);
-//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8', err =>{
-//                 console.log("Your file has been written!!!")
-//             })
-//         })
-//     })
-// })
-
-
-/////////////////////////////////////////////////////
 // SERVER
 const http = require('http');
 const url = require('url');
